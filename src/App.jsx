51914e3373689080a291ab7dfea327e4e5980f82
Esteban Roman <esteban.roman@uao.edu.co>
2024-11-19 16:05:55 -0500

    Base de datos de términos financieros para motor de búsqueda

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,6 @@ import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Homepage from './components/Pages/Homepage';
-import Page1 from './components/Pages/Page1';
-import Page2 from './components/Pages/Page2';
-import Page3 from './components/Pages/Page3';
 import "./styles/global.css";
 import "./styles/styles.css";
 
@@ -14,9 +11,6 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="/page1" element={<Page1 />} />
-        <Route path="/page2" element={<Page2 />} />
-        <Route path="/page3" element={<Page3 />} />
       </Routes>
     </Router>
   );
--- src/components/Pages/Homepage.jsx
+++ src/components/Pages/Homepage.jsx
@@ -1,19 +1,11 @@
 import React from 'react'
-import AboutUs from '../PageContent/AboutUS'
 import FinancialCulture from '../PageContent/FinancialCulture'
 import Objectives from '../PageContent/Objectives'
-import Footer from '../PageContent/Footer'
-import Comments from '../PageContent/Comments'
-import Expertos from '../PageContent/Expertos'
 const Homepage = () => {
     return (
         <div>
-            <AboutUs />
             <Objectives />
             <FinancialCulture />
-            <Footer />
-            <Comments />
-            <Expertos />
         </div>
     )
 }
